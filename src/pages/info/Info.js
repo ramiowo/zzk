69017a493080f9ed8c46de8607d43db389719da1
ramiowo <yerami1@naver.com>
2024-12-16 14:31:44 +0900

    info

--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -24,7 +24,7 @@ const Container = styled.section`
   }
   h3 {
     margin-top: 30px;
-    font-size: 20px;
+    font-size: 18px;
     font-weight: 300;
     margin-bottom: 10px;
     margin-left: 10px;
@@ -46,7 +46,7 @@ const Title = styled.div`
 `;
 
 const UnderLine = styled.div`
-margin: 0 auto;
+  margin: 0 auto;
   width: 296px;
   height: 1px;
   /* margin-bottom: 28px; */
@@ -90,9 +90,10 @@ const Mbti = styled.div`
 `;
 
 const WithWrap = styled.div`
-  margin-top: 60px;
+  margin-top: 40px;
   h3 {
     text-align: center;
+    margin-left: 0;
   }
 `;
 const ButtonGroup = styled.div`
@@ -112,15 +113,15 @@ const ButtonGroup = styled.div`
 
   .active {
     background-color: #ffcccc;
-    box-shadow: inset 0 0 20px rgba(255,255,255,0.3);
-    border: 2px solid rgba(255,255,255,0.4);
+    box-shadow: inset 0 0 20px rgba(255, 255, 255, 0.3);
+    border: 2px solid rgba(255, 255, 255, 0.4);
     color: #fff;
   }
 
   .inactive {
     background-color: #fff;
     color: #ffcccc;
-    box-shadow: inset 0 0 20px rgba(255,255,255,0.3);
+    box-shadow: inset 0 0 20px rgba(255, 255, 255, 0.3);
     border: 2px solid rgba(255, 204, 204, 0.4);
   }
 `;
